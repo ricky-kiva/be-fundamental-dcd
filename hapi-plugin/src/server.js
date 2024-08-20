f5cf47e88dfd8a9318a1b2390eb13c8e60094b93
ricky-kiva <ricky.kiva@gmail.com>
2024-08-21 03:10:01 +0700

    feat(server): impl. `notes` Hapi plugin

--- hapi-plugin/src/server.js
+++ hapi-plugin/src/server.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
-const routes = require('./api/notes/routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
 
 const init = async () => {
+  const notesService = new NotesService();
+
   const server = Hapi.server({
-    port: 5000,
+    port: 3000,
     host: process.env.NODE_ENV !== 'production'
       ? 'localhost'
       : '0.0.0.0',
@@ -16,7 +19,10 @@ const init = async () => {
     }
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: { service: notesService }
+  });
 
   await server.start();
 
