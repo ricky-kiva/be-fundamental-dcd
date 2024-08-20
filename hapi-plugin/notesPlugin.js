a1387c19cd508f1a0211eaa2c95e310523bfea7f
ricky-kiva <ricky.kiva@gmail.com>
2024-08-20 13:24:23 +0700

    first commit

---
+++ hapi-plugin/notesPlugin.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: 'notes',
+  version: '1.0.0',
+  register: async (server, options) => {
+    const notes = options.notes // receive passed option
+    server.route(
+      [
+        {
+          method: 'GET',
+          path: '/notes',
+          handler: () => notes
+        }
+      ]
+    )
+  }
+}
---
+++ hapi-plugin/server.js
@@ -0,0 +1,15 @@
+const notesPlugin = require('./notesPlugin')
+const Hapi = require('@hapi/hapi')
+
+const init = async () => {
+  const server = Hapi.server()
+
+  await server.register({ // can register another plugin here
+    plugin: notesPlugin,
+    options: { notes: [] } // assign option
+  })
+
+  await server.start()
+}
+
+init()
