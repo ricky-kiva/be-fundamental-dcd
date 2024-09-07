8386d0b6ad977156792235e863a5564ff8932293
ricky-kiva <ricky.kiva@gmail.com>
2024-09-07 11:30:41 +0700

    feat(migration): add migration for table `notes`

---
+++ hapi-plugin/migrations/1725681966772_create-table-notes.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('notes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true
+    },
+    tags: {
+      type: 'TEXT[]',
+      notNull: true
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true
+    }
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('notes');
+};
