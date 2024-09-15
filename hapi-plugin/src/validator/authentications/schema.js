9c39912b304c104a504bbc8006667d97cbb5c395
ricky-kiva <ricky.kiva@gmail.com>
2024-09-16 00:24:24 +0700

    feat(validator): add `authentications` validator

---
+++ hapi-plugin/src/validator/authentications/schema.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Joi = require('joi');
+
+const PostAuthenticationPayloadSchema = Joi.object({
+  username: Joi.string().required(),
+  password: Joi.string().required()
+});
+
+const PutAuthenticationPayloadSchema = Joi.object({
+  refreshToken: Joi.string().required()
+});
+
+const DeleteAuthenticationPayloadSchema = Joi.object({
+  refreshToken: Joi.string().required()
+});
+
+module.exports = {
+  PostAuthenticationPayloadSchema,
+  PutAuthenticationPayloadSchema,
+  DeleteAuthenticationPayloadSchema
+};
