6a6c4229602543def5cc27cf527d8c0c50d0fe16
ricky-kiva <ricky.kiva@gmail.com>
2024-09-15 20:43:50 +0700

    feat(service): add `users` service

---
+++ hapi-plugin/src/services/postgres/UsersService.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const bcrypt = require('bcrypt');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+
+class UsersService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addUser({ username, password, fullname }) {
+    await this.verifyUsername(username);
+
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const q = {
+      text: 'INSERT INTO notes_users VALUES($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname]
+    };
+
+    const result = await this._pool.query(q);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Failed to add user');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getUserById(userId) {
+    const q = {
+      text: 'SELECT id, username, fullname FROM notes_users WHERE id = $1',
+      values: [userId]
+    };
+
+    const result = await this._pool.query(q);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('User not found');
+    }
+
+    return result.rows[0];
+  }
+
+  async verifyUsername(username) {
+    const q = {
+      text: 'SELECT username FROM notes_users WHERE username = $1',
+      values: [username]
+    };
+
+    const result = await this._pool.query(q);
+
+    if (result.rows.length > 0) {
+      throw new InvariantError('Failed to add user. Username is used');
+    }
+
+    return result.rows[0];
+  }
+}
+
+module.exports = UsersService;
