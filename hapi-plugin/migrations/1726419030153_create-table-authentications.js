51c0b5027f62ab4f6edc7996797bd94ff425430b
ricky-kiva <ricky.kiva@gmail.com>
2024-09-15 23:54:51 +0700

    feat(migration): add migration for `authentications`

---
+++ hapi-plugin/migrations/1726419030153_create-table-authentications.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('notes_authentications', {
+    token: {
+      type: 'TEXT',
+      notNull: true
+    }
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('notes_authentications');
+};
