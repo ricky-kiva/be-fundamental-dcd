2412dff15c6ff7153df9c2f0c49e6189f2a32214
ricky-kiva <ricky.kiva@gmail.com>
2024-09-18 19:02:03 +0700

    feat(validator): add validator for `collaborations`

---
+++ hapi-plugin/src/validator/collaborations/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const InvariantError = require('../../exceptions/InvariantError');
+const { CollaborationPayloadSchema } = require('./schema');
+
+const CollaborationsValidator = {
+  validateCollaborationPayload: (payload) => {
+    const validationResult = CollaborationPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  }
+};
+
+module.exports = CollaborationsValidator;
---
+++ hapi-plugin/src/validator/collaborations/schema.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Joi = require('joi');
+
+const CollaborationPayloadSchema = Joi.object({
+  noteId: Joi.string().required(),
+  userId: Joi.string().required()
+});
+
+module.exports = { CollaborationPayloadSchema };
