b3e6e9f3917b6cea2b0dca190c7e062b9dadafb6
ricky-kiva <ricky.kiva@gmail.com>
2024-09-21 17:36:26 +0700

    feat(service): add `storage` service

---
+++ hapi-plugin/src/services/storage/StorageService.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const fs = require('fs');
+const { resolve } = require('path');
+
+class StorageService {
+  constructor(folder) {
+    this._folder = folder;
+
+    if (!fs.existsSync(folder)) {
+      fs.mkdirSync(folder, { recursive: true });
+    }
+  }
+
+  writeFile(file, meta) {
+    const filename = +new Date() + meta.filename;
+    const path = `${this._folder}/${filename}`;
+
+    const fileStream = fs.createWriteStream(path);
+
+    return new Promise((_, reject) => {
+      fileStream.on('error', (e) => reject(e));
+      file.pipe(fileStream);
+      file.on('end', () => resolve(filename));
+    });
+  }
+}
+
+module.exports = StorageService;
