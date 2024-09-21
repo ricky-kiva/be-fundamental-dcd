566ca5030e49e62dfacd10350b13a522782423d6
ricky-kiva <ricky.kiva@gmail.com>
2024-09-21 15:09:54 +0700

    feat(validator): add `exports notes` payload validator

---
+++ hapi-plugin/src/validator/exports/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const ExportNotesPayloadSchema = require('./schema');
+const InvariantError = require('../../exceptions/InvariantError');
+
+const ExportsValidator = {
+  validateExportsNotesPayload: (payload) => {
+    const validationResult = ExportNotesPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  }
+};
+
+module.exports = ExportsValidator;
---
+++ hapi-plugin/src/validator/exports/schema.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Joi = require('joi');
+
+const ExportNotesPayloadSchema = Joi.object({
+  targetEmail: Joi.string().email({ tlds: true }).required()
+});
+
+module.exports = ExportNotesPayloadSchema;
