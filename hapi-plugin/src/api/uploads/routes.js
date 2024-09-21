376de30eeb1f740ab75df57f460253a1780e0098
ricky-kiva <ricky.kiva@gmail.com>
2024-09-21 20:43:54 +0700

    feat(api): add endpoint to serve uploaded static image

--- hapi-plugin/src/api/uploads/routes.js
+++ hapi-plugin/src/api/uploads/routes.js
@@ -1,17 +1,27 @@
 'use strict';
 
-const uploadImagesPath = '/upload/images';
+const path = require('path');
+
+const uploadPath = '/upload';
 
 const routes = (h) => [
   {
     method: 'POST',
-    path: uploadImagesPath,
+    path: `${uploadPath}/images`,
     handler: h.postUploadImageHandler,
     options: {
       payload: {
         allow: 'multipart/form-data',
         multipart: true,
         output: 'stream'
+      }
+    }
+  }, {
+    method: 'GET',
+    path: `${uploadPath}/{param*}`,
+    handler: {
+      directory: {
+        path: path.resolve(__dirname, 'file')
       }
     }
   }
