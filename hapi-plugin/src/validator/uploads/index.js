49427f3f1f0694d3e477961ab3180a3f3c0b5a86
ricky-kiva <ricky.kiva@gmail.com>
2024-09-21 17:36:36 +0700

    feat(validator): add `uploads` validator

---
+++ hapi-plugin/src/validator/uploads/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { ImageHeadersSchema } = require('./schema');
+const InvariantError = require('../../exceptions/InvariantError');
+
+const UploadsValidator = {
+  validateImageHeaders: (headers) => {
+    const validationResult = ImageHeadersSchema.validate(headers);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  }
+};
+
+module.exports = UploadsValidator;
